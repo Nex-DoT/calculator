992d70357246317c22ac19e8fa07128fd6a5c095
nex-DoT <nex.dot2@gmail.com>
2023-02-11 10:16:57 +0330

    calculator -v1.0

--- javasc.js
+++ javasc.js
@@ -5,13 +5,17 @@ const p = document.querySelector('p'),
 
 butn.forEach((btn)=>{
      
-    let id = +btn.id;
+    let id = btn.id;
     p.innerHTML="";
      btn.addEventListener('click' , function(){
-        if(btn.id== "AC"){
-            p.innerHTML = 0
-        }else if(btn.id == '='){
-            
+        if(btn.id=== "AC"){
+            p.innerHTML = "";
+        }else if(btn.id === '='){
+            p.innerHTML = eval(p.innerHTML);
+        }else if(btn.id ==="DE"){
+            p.innerHTML = p.innerHTML.slice(0,-1);
+        }else{
+            p.innerHTML += id ;
         }
      })
 })
